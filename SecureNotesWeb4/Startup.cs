e822ecc0d7ce93cb41f9e41ebc32a394abfae700
William J. Charbonnet <wjcharbonnet@pilotcat.com>
2018-09-10 17:15:30 -0500

    Updated Statup

--- SecureNotesWeb4/Startup.cs
+++ SecureNotesWeb4/Startup.cs
@@ -11,6 +11,9 @@ using Microsoft.Extensions.DependencyInjection;
 using SecureNotesWeb4.Data;
 using SecureNotesWeb4.Models;
 using SecureNotesWeb4.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace SecureNotesWeb4
 {
@@ -32,6 +35,37 @@ namespace SecureNotesWeb4
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
+
+            // For example only! Don't store your shared keys as strings in code.
+            // Use environment variables or the .NET Secret Manager instead.
+            var sharedKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("mysupers3cr3tsharedkey!"));
+
+
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = "yourAuthorizationServerAddress";
+                    options.Audience = "yourAudiance";
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        // Clock skew compensates for server time drift.
+                        // We recommend 5 minutes or less:
+                        ClockSkew = TimeSpan.FromMinutes(5),
+                        // Specify the key used to sign the token:
+                        IssuerSigningKey = sharedKey,
+                        RequireSignedTokens = true,
+                        // Ensure the token hasn't expired:
+                        RequireExpirationTime = true,
+                        ValidateLifetime = true,
+                        // Ensure the token audience matches our audience value (default true):
+                        ValidateAudience = true,
+                        ValidAudience = "api://default",
+                        // Ensure the token was issued by a trusted authorization server (default true):
+                        ValidateIssuer = true,
+                        ValidIssuer = "https://{yourOktaDomain}/oauth2/default"
+                    };
+                });
 
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
