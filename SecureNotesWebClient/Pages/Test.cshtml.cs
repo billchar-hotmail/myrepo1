249abb324c9948beaf96222edddf7d25533fb3f5
William J. Charbonnet <wjcharbonnet@pilotcat.com>
2019-01-04 18:14:15 -0600

    bootstrap 4 stuff

---
+++ SecureNotesWebClient/Pages/Test.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SecureNotesWebClient.Pages
+{
+    //[Authorize]
+    public class TestModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "This is a test page.";
+            var s = User.Identity.Name;
+            var s2 = s + "1";
+        }
+    }
+}
